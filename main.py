fcdf32f82e6200ed971fdb000864baf7cf7b9cbd
jsung5381 <jsung5381@naver.com>
2018-07-12 16:46:32 +0000

    Extract the calculation logic into a method

--- main.py
+++ main.py
@@ -4,31 +4,27 @@ from github import Github
 from config import access_token
 
 
-def main():
-    # type(g) == GithubObject
-    gh = Github(access_token)
+def get_total_number_of_commits_by_me(me):
+    """
+    Returns total number of commits by a given user
 
-    # type(user) == AuthenticatedUser
-    user = gh.get_user()
+    :param me: AuthenticatedUser
+    :rtype: integer
+    """
 
     total_number_of_commits_by_me = 0
-    repos = user.get_repos()
-    for repo in repos:
-        # ignore if curr_repo is not owned by me
-        if repo.owner.login != user.login: continue
-        #print('repo.name:', repo.name)
-        
-        number_of_commits_by_me = 0
-        commits = repo.get_commits()
+    for repo in me.get_repos():
 
-        # type(commit) == Commit
-        for commit in commits:
-            #print('commit:', commit)
+        # ignore if curr_repo is not owned by me
+        if repo.owner.login != me.login:
+            continue
+
+        number_of_commits_by_me = 0
+        for commit in repo.get_commits():
 
             # github account that no longer exists tends to have none author
             # accummulate commits that owned by me
-            author = commit.author  # type(author) == NamedUser
-            if author is not None and author.login == user.login:
+            if commit.author is not None and commit.author.login == me.login:
                 number_of_commits_by_me += 1
 
         print('For repo: {}, number_of_commits_by_me: {}'.format(
@@ -37,7 +33,19 @@ def main():
         )
 
         total_number_of_commits_by_me += number_of_commits_by_me
-    print('Total_number_of_commits_by_me:', total_number_of_commits_by_me)
+
+    return total_number_of_commits_by_me
+
+
+def main():
+    # type(g) == GithubObject
+    gh = Github(access_token)
+
+    # type(user) == AuthenticatedUser
+    user = gh.get_user()
+
+    total_number_of_commits_by_me = get_total_number_of_commits_by_me(user)
+    print('total_number_of_commits_by_me:', total_number_of_commits_by_me)
 
 
 if __name__ == '__main__':
