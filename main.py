7a7c67afc14b1086931c3df0de22db6f4938629a
jsung5381 <jsung5381@naver.com>
2018-07-12 16:28:34 +0000

    Refactor main for readability

--- main.py
+++ main.py
@@ -5,29 +5,31 @@ from config import access_token
 
 
 def main():
-    g = Github(access_token)
+    # type(g) == GithubObject
+    gh = Github(access_token)
 
     # type(user) == AuthenticatedUser
-    user = g.get_user()
+    user = gh.get_user()
 
     total_number_of_commits_by_me = 0
     repos = user.get_repos()
     for repo in repos:
         # ignore if curr_repo is not owned by me
-        if repo.owner.login != user.login:
-            continue
+        if repo.owner.login != user.login: continue
+        #print('repo.name:', repo.name)
         
         number_of_commits_by_me = 0
         commits = repo.get_commits()
-        for c in commits:
-            author = c.author
+
+        # type(commit) == Commit
+        for commit in commits:
+            #print('commit:', commit)
 
             # github account that no longer exists tends to have none author
+            # accummulate commits that owned by me
+            author = commit.author  # type(author) == NamedUser
             if author is not None and author.login == user.login:
                 number_of_commits_by_me += 1
-                # type(author) == NamedUser
-                #print('author.name:', author.name)
-                #print('author.email:', author.email)
 
         print('For repo: {}, number_of_commits_by_me: {}'.format(
                 repo.name, number_of_commits_by_me
@@ -35,7 +37,7 @@ def main():
         )
 
         total_number_of_commits_by_me += number_of_commits_by_me
-    print('total_number_of_commits_by_me:', total_number_of_commits_by_me)
+    print('Total_number_of_commits_by_me:', total_number_of_commits_by_me)
 
 
 if __name__ == '__main__':
