8233d713fcaa39cf0b4ce9de41b9a72da4bc035a
jsung5381 <jsung5381@naver.com>
2018-07-27 10:12:02 +0000

    Update config.py and refactor extractor.py accordingly

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+# Github
 base = 'https://api.github.com'
-headers = {'Authorization': 'token (personal access token)'}
-username = 'jsung5381'
+headers = {
+        'Authorization': 'token $(personal-access-token)',
+    }
+
+# Email
+email_username = '$(gmail-username)'
+email_password = '$(gmail-app-password)'
+
